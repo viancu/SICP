494d0e63e29eea8b59957730bda18e79afdf9ef9
viancu <vniancu@gmail.com>
2018-05-19 12:53:27 -0700

    Improved the textbook's approach, works better for smaller numbers.

--- ch1/e1-7.scm
+++ ch1/e1-7.scm
@@ -10,8 +10,11 @@
 (define (average x y)
   (/ (+ x y) 2))
 
+;improved the good-enough? procedure so that it works for much smaller numbers
+;it now checks if the difference between the current guess and the previous-
+;guess is a small fraction of the current guess
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+  (< (/ (abs (- (square guess) x)) guess) 0.001))
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
